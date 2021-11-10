61fd2bdcfcff79e32cf800cd5a4c6f4356314c29
NurainiRosli <nurainirosli01@gmail.com>
2021-11-10 16:26:20 +0800

    Update sistemusrah.sql

--- db/sistemusrah.sql
+++ db/sistemusrah.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 28, 2021 at 02:42 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 7.4.23
+-- Generation Time: Nov 10, 2021 at 09:24 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,114 +24,157 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `attendance`
+-- Table structure for table `admin`
 --
 
-CREATE TABLE `attendance` (
-  `idmasa` int(11) NOT NULL,
-  `noMatrik` varchar(12) NOT NULL,
-  `namaPelajar` varchar(100) NOT NULL,
-  `time` time NOT NULL,
-  `date` date NOT NULL
+CREATE TABLE `admin` (
+  `idAdmin` int(11) NOT NULL,
+  `aname` varchar(200) NOT NULL,
+  `ausername` varchar(200) NOT NULL,
+  `apassword` varchar(200) NOT NULL,
+  `emel` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `attendance`
+-- Dumping data for table `admin`
 --
 
-INSERT INTO `attendance` (`idmasa`, `noMatrik`, `namaPelajar`, `time`, `date`) VALUES
-(4, '18DDT19F1018', 'NURAINI BINTI ROSLI', '22:27:00', '2021-10-26');
+INSERT INTO `admin` (`idAdmin`, `aname`, `ausername`, `apassword`, `emel`) VALUES
+(1, 'SUHAILA ', 'admin1', 'admin1', 'admin1@gmail.com');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `students`
+-- Table structure for table `fasilitator`
 --
 
-CREATE TABLE `students` (
+CREATE TABLE `fasilitator` (
+  `idFasi` int(11) NOT NULL,
+  `fnama` varchar(200) NOT NULL,
+  `fnoMatrik` varchar(200) NOT NULL,
+  `noic` varchar(12) NOT NULL,
+  `fjabatan` varchar(50) NOT NULL,
+  `semester` varchar(50) NOT NULL,
+  `fnoTel` varchar(13) NOT NULL,
+  `fkelas` varchar(10) NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `fasilitator`
+--
+
+INSERT INTO `fasilitator` (`idFasi`, `fnama`, `fnoMatrik`, `noic`, `fjabatan`, `semester`, `fnoTel`, `fkelas`, `username`, `password`) VALUES
+(5, 'AIN', '18DDT17F1888', '010426010556', 'JRKV', '5', '01377252547', 'DDT5A', 'fasi1', 'fasi1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `idKelas` int(11) NOT NULL,
+  `jabatan` int(11) NOT NULL,
+  `namaKelas` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `program`
+--
+
+CREATE TABLE `program` (
+  `idProgram` int(11) NOT NULL,
+  `namaProgram` varchar(200) NOT NULL,
+  `tarikh` date NOT NULL,
+  `masa` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
   `idPelajar` int(11) NOT NULL,
-  `namaPelajar` varchar(100) NOT NULL,
-  `noMatrik` varchar(12) NOT NULL,
-  `jabatan` varchar(50) NOT NULL,
-  `kelas` varchar(50) NOT NULL,
-  `noTel` varchar(13) NOT NULL
+  `pnama` varchar(200) NOT NULL,
+  `pnoMatrik` varchar(12) NOT NULL,
+  `pjabatan` varchar(50) NOT NULL,
+  `pkelas` varchar(50) NOT NULL,
+  `namaFasi` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `students`
---
-
-INSERT INTO `students` (`idPelajar`, `namaPelajar`, `noMatrik`, `jabatan`, `kelas`, `noTel`) VALUES
-(3, 'NURAINI BINTI ROSLI', '18DDT19F1018', 'JTMK', 'DDT1A', '0164132736'),
-(4, 'NUR ALIAH HUSNA ', '18DDT19F1098', 'JPA', 'DPA1A', '0180123334'),
-(5, 'NURUL FADHLIN SAHIRA', '18DDT19F1022', 'JRKV', 'DDF1B', '0191122097');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `level` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `name`, `level`) VALUES
-(5, 'admin1', 'admin1234', 'SUHAILA ISMAIL', 'ADMIN'),
-(8, 'admin2', 'admin1234', 'ROSHADAH', 'ADMIN'),
-(9, 'fasi1', 'fasi1234', 'NUR ATIKAH', 'FASILITATOR');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `attendance`
+-- Indexes for table `admin`
 --
-ALTER TABLE `attendance`
-  ADD PRIMARY KEY (`idmasa`);
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`idAdmin`);
 
 --
--- Indexes for table `students`
+-- Indexes for table `fasilitator`
 --
-ALTER TABLE `students`
+ALTER TABLE `fasilitator`
+  ADD PRIMARY KEY (`idFasi`);
+
+--
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`idKelas`);
+
+--
+-- Indexes for table `program`
+--
+ALTER TABLE `program`
+  ADD PRIMARY KEY (`idProgram`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
   ADD PRIMARY KEY (`idPelajar`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `attendance`
+-- AUTO_INCREMENT for table `admin`
 --
-ALTER TABLE `attendance`
-  MODIFY `idmasa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+ALTER TABLE `admin`
+  MODIFY `idAdmin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `students`
+-- AUTO_INCREMENT for table `fasilitator`
 --
-ALTER TABLE `students`
-  MODIFY `idPelajar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `fasilitator`
+  MODIFY `idFasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table `kelas`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+ALTER TABLE `kelas`
+  MODIFY `idKelas` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `program`
+--
+ALTER TABLE `program`
+  MODIFY `idProgram` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `idPelajar` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
